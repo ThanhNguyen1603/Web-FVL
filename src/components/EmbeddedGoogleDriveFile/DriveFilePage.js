d401cbfb99586bdf94333768e0798da45d66bd32
quockhanh973 <quockhanh973@gmail.com>
2024-12-03 11:58:43 +0700

    up1203

--- src/components/EmbeddedGoogleDriveFile/DriveFilePage.js
+++ src/components/EmbeddedGoogleDriveFile/DriveFilePage.js
@@ -1,33 +1,35 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import React, { useEffect } from "react";
 
 
 const Introduction = () => {
   return (
-    <iframe className="embed-responsive-item"
-      src="https://drive.google.com/file/d/1m_2f_FiacceCI65EZ5BHerJBehp1zFc_/preview"
+    <iframe
+      className="embed-responsive-item"
+      src="https://drive.google.com/file/d/1m_2f_FiacceCI65EZ5BHerJBehp1zFc_/preview?autoplay=1"
       allowFullScreen
       allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
       title="Tệp từ Google Drive"
-      as="script"
+      style={{ width: "100%", height: "675px" }}
     ></iframe>
-  )
-}
+  );
+};
 const WeWorkVideo = () => {
   return (
     <iframe
-      src="https://player.vimeo.com/video/1017348844?h=8373a4ea4a"
+      src="https://player.vimeo.com/video/1017348844?h=8373a4ea4a&autoplay=1"
       className="embed-responsive-item"
       allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
       title="WeWork Video"
-      allowfullscreen
+      allowFullScreen
       style={{ width: '100%', height: '675px' }}
-    ></iframe >
+    ></iframe>
   );
-}
+};
 const WePlayVideo = () => {
   return (
-    <iframe src="https://player.vimeo.com/video/1017011076?h=e82fbfaacb"
+    <iframe src="https://player.vimeo.com/video/1017011076?h=e82fbfaacb&autoplay=1"
       className="embed-responsive-item"
 
       style={{ width: '100%', height: '675px' }} frameborder="0" allowfullscreen="true"
@@ -40,18 +42,20 @@ const WePlayVideo = () => {
 }
 const WeGiveVideo = () => {
   return (
-    <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fturkeydashvietnam%2Fvideos%2F441023589936233%2F&show_text=false&width=560&t=0"
-      className="embed-responsive-item" style={{ width: '100%', height: '675px' }} frameborder="0"
-      allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-      title="Tệp từ Google Drive" as="scipt">
-    </iframe>
-  )
-
-}
+    <iframe
+      src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fturkeydashvietnam%2Fvideos%2F441023589936233%2F&show_text=false&width=560&t=0&autoplay=1"
+      className="embed-responsive-item"
+      style={{ width: '100%', height: '675px' }}
+      allowFullScreen
+      allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+      title="Facebook Video"
+    ></iframe>
+  );
+};
 
 const DreamSmile = () => {
   return (
-    <iframe src="https://player.vimeo.com/video/1031456896?h=4c67ac9925"
+    <iframe src="https://player.vimeo.com/video/1031456896?h=4c67ac9925&autoplay=1"
 
       className="embed-responsive-item" style={{ width: '100%', height: '675px' }} frameborder="0"
       allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
